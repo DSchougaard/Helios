6fd5ae3b7f65bfb2dd87ce1618e0117816b01196
Daniel Schougaard <danielschougaard@gmail.com>
2015-04-08 12:35:01 +0200

    Server now reads content of MongoDB instead of json file.

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ var express = require('express');
 var path = require('path');
 var wol = require('wake_on_lan');
 var bodyParser = require('body-parser');
-var sequest = require('sequest');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+/*
+	Possible Modules?
+	https://www.npmjs.com/package/mongo-sanitize
+	https://www.npmjs.com/package/lodash
+	https://www.npmjs.com/package/async
+	https://www.npmjs.com/package/validator
+		https://github.com/hapijs/joi
+	https://www.npmjs.com/package/node-libnmap
+	https://github.com/bevry/getmac
+
+
+	https://github.com/sindresorhus/awesome-nodejs#logging
+*/
+
 
 /*
 	Own Modules
@@ -16,24 +32,31 @@ var shutdown_ssh = require('./shutdown_ssh');
 
 //  Setup
 var app = express();
-//var mongo = require('mongodb').MongoClient;
 
-/*
-mongo.connect('mongodb://127.0.0.1:27017/helio', function(err,db){
-	if(err) throw err;
 
-	var 
-});
-*/
+// Database Variables
+var db_url = "mongodb://localhost:27017/helios";
+var device_collection = "devicecollection";
 
-// sadasdasasasdsasdasasd
 
 app.use(express.static(__dirname + '/public')); 
 app.use(bodyParser.json());
 //
 app.get('/api/machines', function(req, res){
-	var jsondb = require('./db.json');
-	res.json(jsondb);
+	
+
+	MongoClient.connect(db_url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected correctly to server");
+
+		var collection = db.collection(device_collection);
+		collection.find().toArray(function(err, docs){
+		    console.log("Found the following records");
+		    console.dir(docs);
+		    res.json(docs);
+		    db.close();
+		});
+	});
 });
 
 
