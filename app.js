7889aba32088faca6ace6dae950ed32adbe39c45
Daniel Schougaard <danielschougaard@gmail.com>
2015-04-10 18:54:24 +0200

    Additional route seperation

---
+++ app.js
@@ -0,0 +1,10 @@
+module.exports = function(app, db, device_collection){
+	/*
+		Routes
+	*/
+	require('./routes/device')(app, db, device_collection);
+
+	app.get('/*', function(req, res) {
+		res.sendFile( __dirname + '/public/index.html'); 
+	});
+}
