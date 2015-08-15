643453000736520c2da269de3ede17a98f9ac93b
Daniel Schougaard <danielschougaard@gmail.com>
2015-08-15 20:41:06 +0200

    Table creation queries

---
+++ queries.js
@@ -0,0 +1,40 @@
+/*var template = 
+"CREATE TABLE database_name.table_name(
+   column1 datatype  PRIMARY KEY(one or more columns),
+   column2 datatype,
+   column3 datatype,
+   .....
+   columnN datatype,
+);"
+*/
+
+
+//ID | Name | IP | Mac | authType
+exports.createDeviceTable = "CREATE TABLE IF NOT EXISTS devices(\
+   	id 						INTEGER PRIMARY KEY AUTOINCREMENT,\
+	name 					TEXT				NOT NULL, \
+	ip 						TEXT 				NOT NULL, \
+	mac 					TEXT 				NOT NULL, \
+	auth_type 				TEXT 				NOT NULL \
+);"
+
+
+//ID | deviceID| username | storePassword | password
+exports.createPasswordTable = "CREATE TABLE IF NOT EXISTS password_auth(\
+	id 						INT PRIMARY KEY		NOT NULL, \
+	device_id				INT					NOT NULL, \
+	username 				TEXT				NOT NULL, \
+	storePassword			INT					NOT NULL, \
+	password 				TEXT 				NOT NULL, \
+	FOREIGN KEY(device_id) REFERENCES devices(id) \
+);"
+
+
+//ID | deviceID | Cert
+exports.createCertTable = "CREATE TABLE IF NOT EXISTS cert_auth(\
+	id 						INT PRIMARY KEY		NOT NULL, \
+	device_id 				INT 				NOT NULL, \
+	cert 					BLOB 				NOT NULL, \
+	FOREIGN KEY(device_id) REFERENCES devices(id) \
+);"
+
