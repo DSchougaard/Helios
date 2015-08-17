37e21e11f04fd1cd19aabe49a73458fc29dfb6be
Daniel Schougaard <danielschougaard@gmail.com>
2015-08-17 21:14:09 +0200

    Added scanning routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ module.exports = function(app, db, device_collection){
 		Routes
 	*/
 	require('./routes/device')(app, db, device_collection);
+	require('./routes/scan')(app, db);
 
 	app.get('*', function(req, res) {
 		res.sendFile( __dirname + '/public/index.html'); 
