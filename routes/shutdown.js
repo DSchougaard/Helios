37291343a730d391740d317b4d8b99ba0d77123a
Daniel Schougaard <danielschougaard@gmail.com>
2015-11-07 23:30:09 +0100

    Express response in API::Shutdown fixed.

--- routes/shutdown.js
+++ routes/shutdown.js
@@ -26,14 +26,14 @@ module.exports = function(app, db){
 			}else if( user.username == null && user.password != null && req.body.payload.device.ssh_username != null ){
 				sshShutdown.shutdown(device, device.ssh_username, user.password);
 				console.log("API::Shutdown:: Device with mac %s was shut down.", device.mac);
-				res.status(200).send("OK");
+				res.send(200).send("OK");
 			}else if( device.ssh_username == null && user.username != null && user.password != null ){
 				sshShutdown.shutdown(device, user.username, user.password);
 				console.log("API::Shutdown:: Device with mac %s was shut down.", device.mac);
-				res.status(200).send("OK");
+				res.send(200).send("OK");
 			}else{
 				console.log("API::Shutdown:: Error. Payload contained no suitable authentication details.");
-				res.status(400).send("Error: Payload contained no suitable authentication details.");
+				res.send(400).send("Error: Payload contained no suitable authentication details.");
 			}
 		});
 
